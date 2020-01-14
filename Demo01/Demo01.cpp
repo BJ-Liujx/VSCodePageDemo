e21c9c97547c66db9cdbc9fcbe4797205057142d
BJ_liujx <BJ_liujx@163.com>
2020-01-14 16:18:10 +0800

    1

---
+++ Demo01/Demo01.cpp
@@ -0,0 +1,10 @@
+//这是一段注释
+#include <iostream> //预处理器编译指令#include
+int main()//函数头文件
+{
+    using namespace std;//编译指令
+    cout << "Hello World!";
+    cout << endl;
+    cout << "Hello World2" << endl;
+    return 0;//main函数的返回值
+}
---
+++ Demo01/Demo02.cpp
@@ -0,0 +1,11 @@
+//测试C++模板可用
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+   system("pause");
+   return 0;
+}
